dbc569580993328411d290460b5f7cc04898cf79
shakiz <activities335@gmail.com>
2019-10-15 21:11:50 +0600

    handled the session.

--- app_dir/main/views.py
+++ app_dir/main/views.py
@@ -61,6 +61,7 @@ def loginUser(request):
 
 
 def logout_user(request):
+    request.session['username'] = ''
     logout(request)
     messages.info(request, "Logged out successfully!")
     return redirect("main:register")
@@ -87,71 +88,76 @@ def save_composition(request):
 
 
 def home(request):
-    all_service = requests.get("http://127.0.0.1:8000/api/service_registry/").json()
+    print(request.session['username'])
+    if request.session['username'] is None:
+        return render(request, 'login.html')
 
-    if len(all_service) > 0:
-        last_service = all_service[-1]
-        read_file = open('registry.txt', 'r')
-        a = read_file.read()
-        read_file.close()
-        name_id = last_service['name']
-        sensor_tag = name_id
-        name_id = name_id.replace(".", "_")
+    else:
+        all_service = requests.get("http://127.0.0.1:8000/api/service_registry/").json()
 
-        if int(a) < last_service['id']:
-            write_file = open('registry.txt', 'w+')
-            write_file.write(str(last_service['id']))
-            write_file.close()
-            if last_service['service_type'] == 'sensor':
-                function_writer.sensor_function_writer(name_id, sensor_tag)
-                block_generator.block_generator(name_id, last_service['service_type'])
-                form_creator.sensor_interface_creator(name_id)
+        if len(all_service) > 0:
+            last_service = all_service[-1]
+            read_file = open('registry.txt', 'r')
+            a = read_file.read()
+            read_file.close()
+            name_id = last_service['name']
+            sensor_tag = name_id
+            name_id = name_id.replace(".", "_")
 
-            elif last_service['service_type'] == 'actuator':
-                function_writer.actuator_function_writer(name_id, sensor_tag)
-                block_generator.block_generator(name_id, last_service['service_type'])
-                form_creator.actuator_interface_creator(name_id)
+            if int(a) < last_service['id']:
+                write_file = open('registry.txt', 'w+')
+                write_file.write(str(last_service['id']))
+                write_file.close()
+                if last_service['service_type'] == 'sensor':
+                    function_writer.sensor_function_writer(name_id, sensor_tag)
+                    block_generator.block_generator(name_id, last_service['service_type'])
+                    form_creator.sensor_interface_creator(name_id)
 
-            elif last_service['service_type'] == 'api':
-                last_api = API.objects.get(name=name_id)
-                api_name = last_api.name
-                api_link = last_api.api
-                api_fields = last_api.fields
-                api_connection = last_api.connection
-                api_name = api_name.replace(".", "_")
-                function_writer.api_function_writer(api_name, api_link, api_fields)
-                block_generator.api_block_generator(api_name, 'api', api_fields, api_connection)
-                form_creator.api_form_creator(api_name, api_fields)
-                form_creator.url_creator(api_name)
-                form_creator.post_function_creator(api_name, api_link, api_fields)
+                elif last_service['service_type'] == 'actuator':
+                    function_writer.actuator_function_writer(name_id, sensor_tag)
+                    block_generator.block_generator(name_id, last_service['service_type'])
+                    form_creator.actuator_interface_creator(name_id)
 
-            elif last_service['service_type'] == 'interactive':
-                last_interactive = Interactive.objects.get(name=name_id)
-                interactive_name = last_interactive.name
-                interactive_fields = last_interactive.fields
-                interactive_name = interactive_name.replace(".", "_")
-                function_writer.interactive_function_writer(interactive_name, interactive_fields)
-                block_generator.interactive_block_generator(interactive_name, 'interactive', interactive_fields)
+                elif last_service['service_type'] == 'api':
+                    last_api = API.objects.get(name=name_id)
+                    api_name = last_api.name
+                    api_link = last_api.api
+                    api_fields = last_api.fields
+                    api_connection = last_api.connection
+                    api_name = api_name.replace(".", "_")
+                    function_writer.api_function_writer(api_name, api_link, api_fields)
+                    block_generator.api_block_generator(api_name, 'api', api_fields, api_connection)
+                    form_creator.api_form_creator(api_name, api_fields)
+                    form_creator.url_creator(api_name)
+                    form_creator.post_function_creator(api_name, api_link, api_fields)
 
-    # MailBox Creator
-    mailboxes = MailBox.objects.all()
-    mailbox_len = mailboxes.count()
-    if mailbox_len > 0:
-        last_mailbox = mailboxes[mailbox_len - 1]
-        read_mailbox = open('mailbox.txt', 'r')
-        mailbox_a = read_mailbox.read()
-        read_mailbox.close()
-        mailbox_name = last_mailbox.name
-        mailbox_tag = last_mailbox.api
-        mailbox_name = mailbox_name.replace(".", "_")
-        if int(mailbox_a) < last_mailbox.id:
-            write_file = open('mailbox.txt', 'w+')
-            write_file.write(str(last_mailbox.id))
-            write_file.close()
-            function_writer.mailbox_function_writer(mailbox_name, mailbox_tag.name)
-            block_generator.block_generator(mailbox_name, 'mailbox')
+                elif last_service['service_type'] == 'interactive':
+                    last_interactive = Interactive.objects.get(name=name_id)
+                    interactive_name = last_interactive.name
+                    interactive_fields = last_interactive.fields
+                    interactive_name = interactive_name.replace(".", "_")
+                    function_writer.interactive_function_writer(interactive_name, interactive_fields)
+                    block_generator.interactive_block_generator(interactive_name, 'interactive', interactive_fields)
 
-    return render(request, 'index.html')
+        # MailBox Creator
+        mailboxes = MailBox.objects.all()
+        mailbox_len = mailboxes.count()
+        if mailbox_len > 0:
+            last_mailbox = mailboxes[mailbox_len - 1]
+            read_mailbox = open('mailbox.txt', 'r')
+            mailbox_a = read_mailbox.read()
+            read_mailbox.close()
+            mailbox_name = last_mailbox.name
+            mailbox_tag = last_mailbox.api
+            mailbox_name = mailbox_name.replace(".", "_")
+            if int(mailbox_a) < last_mailbox.id:
+                write_file = open('mailbox.txt', 'w+')
+                write_file.write(str(last_mailbox.id))
+                write_file.close()
+                function_writer.mailbox_function_writer(mailbox_name, mailbox_tag.name)
+                block_generator.block_generator(mailbox_name, 'mailbox')
+
+        return render(request, 'index.html')
 
 
 class ActuatorAPI(viewsets.ModelViewSet):
